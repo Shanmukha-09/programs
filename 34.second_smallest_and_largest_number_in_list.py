854d003724a067c8b88db06ad5d184c72d449aac
Shanmukha-09 <kunapareddysuresh41@gmail.com>
2024-07-15 14:44:51 +0530

    Create 34.second_smallest_and_largest_number_in_list.py

---
+++ 34.second_smallest_and_largest_number_in_list.py
@@ -0,0 +1,7 @@
+def function(list):
+   length=len(list)
+   list.sort()
+print("Second largest is:",list[length-2])
+print("Second smallest is:",list[1])
+list=[12,45,16,17,18,20]
+large=find(list)
